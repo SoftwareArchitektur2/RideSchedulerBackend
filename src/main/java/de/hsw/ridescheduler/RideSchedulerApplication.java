1a15063a04be998b5972c9e6b0b429b8a89369b4
jpfrehe <Jp.Frehe@googlemail.com>
2022-02-08 12:28:41 +0100

    Initial Commit

---
+++ src/main/java/de/hsw/ridescheduler/RideSchedulerApplication.java
@@ -0,0 +1,13 @@
+package de.hsw.ridescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hsw/ridescheduler/RideSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsw.ridescheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
