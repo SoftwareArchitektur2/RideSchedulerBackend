b1525293d83c4cecd8fb2af71fcb3514251f91ab
Jan Philip Frehe <64253414+jpfrehe@users.noreply.github.com>
2022-02-28 12:08:16 +0100

    Enhance cors for 4 endpoints

--- src/main/java/de/hsw/ridescheduler/RideSchedulerApplication.java
+++ src/main/java/de/hsw/ridescheduler/RideSchedulerApplication.java
@@ -27,7 +27,7 @@ public class RideSchedulerApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/****").allowedOrigins("http://localhost:3000");
 			}
 		};
 	}
