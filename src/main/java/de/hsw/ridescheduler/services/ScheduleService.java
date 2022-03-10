22bde5c3b2d4bb5305af742df3355fb60ddbfc49
Jan Philip Frehe <64253414+jpfrehe@users.noreply.github.com>
2022-03-10 09:02:09 +0100

    Sort result for journey planner by arrival time

--- src/main/java/de/hsw/ridescheduler/services/ScheduleService.java
+++ src/main/java/de/hsw/ridescheduler/services/ScheduleService.java
@@ -136,6 +136,7 @@ public class ScheduleService {
                 }
             }
         }
+        result.sort(Comparator.comparing(ScheduleResponse::getDepartureTime));
         return result;
     }
 
